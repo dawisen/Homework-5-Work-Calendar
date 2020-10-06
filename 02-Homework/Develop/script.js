03d4ba1fefc79f0d8da24f121919387c3b8213f9
gudetama <daniellewwise@gmail.com>
2020-10-05 22:43:17 -0400

    added local storage functionality

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,17 +1,70 @@
-// $(document).ready(){
-// }
-
-  // display value of moment object
-// var eDisplayMoment = $("#currentDay");
-// $(eDisplayMoment).html(moment().format("MMMM Do YYYY, h:mm:ss a"));
-
-
+var mySchedule = {};
+// displays current day
 $("#currentDay").html("Today is " + moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-//local time?
-console.log(moment().format('LLLL'));
+$(document).ready(function () {
+    checkSchedule();
 
+    $(".saveBtn").on("click", function () {
+      var thisTimeblock = $(this).parent().closest("div[id]");
+      var timeBlockID = thisTimeblock.attr("id");
+      console.log("timeblock", timeBlockID);
+      console.log(
+        "saved text input: ",
+        thisTimeblock.find($("textarea")).val()
+      );
+      mySchedule[timeBlockID] = thisTimeblock.find($("textarea")).val();
+      console.log(mySchedule);
+      localStorage.setItem("Schedule", JSON.stringify(mySchedule));
+    });
 
-// relative time
-// moment().startOf("day").fromNow();
-// moment().endOf("day").fromNow();
+  function scheduleIsEmpty() {
+      if (localStorage.getItem("Schedule") === null) {
+        return true;
+      } else {
+        return false;
+      }
+  }
+
+  function checkSchedule() {
+      if (scheduleIsEmpty() === false) {
+        mySchedule = JSON.parse(localStorage.getItem("Schedule"));
+        console.log(mySchedule);
+        getStoredText();
+      } else {
+        console.log("the sched is empty");
+      }
+  }
+  
+  function getStoredText() {
+    if (mySchedule.hasOwnProperty('nine') === true) {
+      $("#nine").find($("textarea")).val(mySchedule["nine"]);
+    }
+    if (mySchedule.hasOwnProperty('ten') === true) {
+      $("#ten").find($("textarea")).val(mySchedule["ten"]);
+    }
+    if (mySchedule.hasOwnProperty('eleven') === true) {
+      $("#eleven").find($("textarea")).val(mySchedule["eleven"]);
+    }
+    if (mySchedule.hasOwnProperty('twelve') === true) {
+      $("#twelve").find($("textarea")).val(mySchedule["twelve"]);
+    }
+    if (mySchedule.hasOwnProperty('one') === true) {
+      $("#one").find($("textarea")).val(mySchedule["one"]);
+    }
+    if (mySchedule.hasOwnProperty('two') === true) {
+      $("#two").find($("textarea")).val(mySchedule["two"]);
+    }
+    if (mySchedule.hasOwnProperty('three') === true) {
+      $("#three").find($("textarea")).val(mySchedule["three"]);
+    }
+    if (mySchedule.hasOwnProperty('four') === true) {
+      $("#four").find($("textarea")).val(mySchedule["four"]);
+    }
+    if (mySchedule.hasOwnProperty('five') === true) {
+      $("#five").find($("textarea")).val(mySchedule["five"]);
+    }
+  }
+})
+
+  
