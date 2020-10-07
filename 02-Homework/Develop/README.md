05cacf92c0da713eacaa0826cd0a18b147246076
gudetama <daniellewwise@gmail.com>
2020-10-06 22:16:44 -0400

    adding momentjs code for color changes

---
+++ 02-Homework/Develop/README.md
@@ -0,0 +1,17 @@
+# Homework-5-Work-Calendar
+
+
+
+
+# Functionality:
+- When you open the planner the current day is displayed at the top of the calendar using moment.js
+
+- WHEN you scroll down you are presented with time blocks for standard business hours (9am-5pm)
+
+- The time blocks are color-coded to indicate whether it is in the past (RED), present(GREEN), or future (GRAY)
+
+- When you click on a time block you can enter an event
+
+- You can click the save button for that time block to save your memo in local storage
+
+- The saved events persist in local storage when you refresh the page
--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var mySchedule = {};
-// displays current day
-$("#currentDay").html("Today is " + moment().format("MMMM Do YYYY, h:mm:ss a"));
 
 $(document).ready(function () {
-    checkSchedule();
+  $("#currentDay").html("Today is " + moment().format("MMMM Do YYYY, h:mm:ss a"));// displays current day
+
+  checkTime();
+  checkSchedule();
 
     $(".saveBtn").on("click", function () {
       var thisTimeblock = $(this).parent().closest("div[id]");
       var timeBlockID = thisTimeblock.attr("id");
+
       console.log("timeblock", timeBlockID);
       console.log(
         "saved text input: ",
@@ -37,34 +39,55 @@ $(document).ready(function () {
   }
   
   function getStoredText() {
-    if (mySchedule.hasOwnProperty('nine') === true) {
-      $("#nine").find($("textarea")).val(mySchedule["nine"]);
+    if (mySchedule.hasOwnProperty('9') === true) {
+      $("#9").find($("textarea")).val(mySchedule["9"]);
     }
-    if (mySchedule.hasOwnProperty('ten') === true) {
-      $("#ten").find($("textarea")).val(mySchedule["ten"]);
+    if (mySchedule.hasOwnProperty('10') === true) {
+      $("#10").find($("textarea")).val(mySchedule["10"]);
     }
-    if (mySchedule.hasOwnProperty('eleven') === true) {
-      $("#eleven").find($("textarea")).val(mySchedule["eleven"]);
+    if (mySchedule.hasOwnProperty('11') === true) {
+      $("#11").find($("textarea")).val(mySchedule["11"]);
     }
-    if (mySchedule.hasOwnProperty('twelve') === true) {
-      $("#twelve").find($("textarea")).val(mySchedule["twelve"]);
+    if (mySchedule.hasOwnProperty('12') === true) {
+      $("#12").find($("textarea")).val(mySchedule["12"]);
     }
-    if (mySchedule.hasOwnProperty('one') === true) {
-      $("#one").find($("textarea")).val(mySchedule["one"]);
+    if (mySchedule.hasOwnProperty('1') === true) {
+      $("#1").find($("textarea")).val(mySchedule["1"]);
     }
-    if (mySchedule.hasOwnProperty('two') === true) {
-      $("#two").find($("textarea")).val(mySchedule["two"]);
+    if (mySchedule.hasOwnProperty('2') === true) {
+      $("#2").find($("textarea")).val(mySchedule["2"]);
     }
-    if (mySchedule.hasOwnProperty('three') === true) {
-      $("#three").find($("textarea")).val(mySchedule["three"]);
+    if (mySchedule.hasOwnProperty('3') === true) {
+      $("#3").find($("textarea")).val(mySchedule["3"]);
     }
-    if (mySchedule.hasOwnProperty('four') === true) {
-      $("#four").find($("textarea")).val(mySchedule["four"]);
+    if (mySchedule.hasOwnProperty('4') === true) {
+      $("#4").find($("textarea")).val(mySchedule["4"]);
     }
-    if (mySchedule.hasOwnProperty('five') === true) {
-      $("#five").find($("textarea")).val(mySchedule["five"]);
+    if (mySchedule.hasOwnProperty('5') === true) {
+      $("#5").find($("textarea")).val(mySchedule["5"]);
     }
   }
+
+  function checkTime() {
+    var currentTime = moment().format("H");
+    var timeBlockElements = $(".timeblock").find($("textarea"));
+    for (var index = 0; index < timeBlockElements.length; index++) {
+      var elementID = timeBlockElements[index].id;
+      var manipID = $(timeBlockElements[index]).attr("id");
+      $(timeBlockElements[index].id).removeClass(".present .past .future");
+
+      if (elementID < currentTime) {
+        $(manipID).addClass("past");
+      } else if (elementID > currentTime) {
+        $(manipID).addClass("future");
+      } else {
+        $(manipID).addClass("present");
+      }
+    }
+  }
+  setInterval(checkTime(), 1000 * 60 * 5);
 })
 
   
+
+//https://stackoverflow.com/questions/62462599/how-can-i-change-elements-style-based-on-the-time-of-day
